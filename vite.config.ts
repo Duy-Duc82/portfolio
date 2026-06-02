f6649a3896b630027922ee6b42b1dfbb8705a6d6
Dương Đức Duy <duongducduy825@gmail.com>
2026-06-02 18:16:20 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 export default defineConfig({
+    base: '/portfolio/',
     plugins: [react(), visualizer() as PluginOption],
     resolve: {
         alias: {
